260dc28515587a190af5b1080a7a1a9cd51072d2
julien <julien.da-silva@hetic.net>
2018-07-03 09:10:06 +0200

    Initial Commit

---
+++ healer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  healer
+//
+//  Created by julien da silva on 03/07/2018.
+//  Copyright © 2018 julien da silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
