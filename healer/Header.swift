e126afc3dceb48f08dc04557cb777f8c39754753
Sara-attallah <attallah.sara@gmail.com>
2018-07-03 16:12:00 +0200

    create header

---
+++ healer/Header.swift
@@ -0,0 +1,19 @@
+//
+//  Header.swift
+//  healer
+//
+//  Created by Sara on 03/07/2018.
+//  Copyright © 2018 julien da silva. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Header: UIView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
---
+++ healer/QuestionHeader.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionHeader.swift
+//  healer
+//
+//  Created by Sara on 03/07/2018.
+//  Copyright © 2018 julien da silva. All rights reserved.
+//
+
+import Foundation
+
+class QuestionHeader: UIView? {
+}
